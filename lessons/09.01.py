73f79cf4cfe4094cbeda34c96a802cde24a364aa
DmytroPKT <demetriusz.pk@gmail.com>
2023-02-09 12:12:36 +0200

    functions create & docing

---
+++ lessons/09.01.py
@@ -0,0 +1,42 @@
+"""
+функции
+def имя_функции(аргумент(Ы)):
+    строка документации
+    тело функции(операторы)(код)
+"""
+
+def function_name():
+    """some string with describe function"""
+    pass
+
+function_name()
+print(function_name.__doc__)
+
+def print_hello_message():
+    """
+    printing hello message
+    :return:
+    """
+    print('Hello!')
+
+def print_named_hello_message(name):
+    """
+    printing named hello message
+    :param name:
+    :return:
+    """
+
+    print(f'Hello {name}!')
+
+def calc(num1, op, num2):
+    """
+    comments
+    :param num1:
+    :param op:
+    :param num2:
+    :return:
+    """
+    return eval(f'{num1}{op}{num2}')
+
+result = calc(2, '+', 5)
+print(result)
